b8d23229be78fd698638a3c99299ec2285961eb9
Colin Ian King <colin.i.king@gmail.com>
2025-07-08 09:10:12 +0100

    Add copyright and control-c message
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- smi.c
+++ smi.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2021 Canonical
+ * Copyright (C) 2021-2025 Colin Ian King
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
 #include <sys/io.h>
 #include <sys/time.h>
 #include <unistd.h>
@@ -136,6 +155,8 @@ int main(void)
 		fprintf(stderr, "Cannot access port 0xb2\n");
 		exit(EXIT_FAILURE);
 	}
+
+	printf("hit control-c to stop..\n");
 	
 	for (;;) {
 		int i;
